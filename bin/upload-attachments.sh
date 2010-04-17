8d754a39a650fa17760e5edd58cb8745ee349ba9
Simon de Haan <simon@soocial.com>
2010-04-17 23:35:45 +0200

    working on rendering templates, failing ...

--- bin/upload-attachments.sh
+++ bin/upload-attachments.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 source config.source
-REV=`./get_revision_for.sh _design/analytics`
 cd $WORKING_DIR/_attachments/
 for FILE in *
 do
-    curl -X PUT $COUCH_INSTANCE/_design/analytics/$FILE?rev=$REV -d @$FILE
+    cd $WORKING_DIR/bin/
+    REV=`./get_revision_for.sh _design/analytics`
+    cd $WORKING_DIR/_attachments/
+    MIME_TYPE=`file --mime --brief $FILE`
+    curl -X PUT $COUCH_INSTANCE/_design/analytics/$FILE?rev=$REV -d @$FILE -H "Content-Type: $MIME_TYPE"
 done
 cd $WORKING_DIR/bin/
